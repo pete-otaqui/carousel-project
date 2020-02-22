f6437f6c097d11c3596215f64e78536e9110d5e4
Pete Otaqui <pete@otaqui.com>
2020-02-22 16:44:14 +0000

    Added navigation and tests for carousel

--- packages/generic-carousel/src/generic-carousel.test.tsx
+++ packages/generic-carousel/src/generic-carousel.test.tsx
@@ -1,10 +1,98 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import { GenericCarousel } from "./generic-carousel";
+import { GenericCarousel, GCItem } from "./generic-carousel";
+
+const allItems: GCItem[] = [
+  { guid: "1", src: "src-1.jpg", title: "title 1" },
+  { guid: "2", src: "src-2.jpg", title: "title 2" },
+  { guid: "3", src: "src-3.jpg", title: "title 3" },
+  { guid: "4", src: "src-4.jpg", title: "title 4" },
+  { guid: "5", src: "src-5.jpg", title: "title 5" },
+  { guid: "6", src: "src-6.jpg", title: "title 6" },
+  { guid: "7", src: "src-7.jpg", title: "title 7" }
+];
+
+const sleep = async (ms: number): Promise<void> => {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+};
 
 describe("Generic Carousel", () => {
-  it("should render", () => {
-    expect(shallow(<GenericCarousel items={[]} />)).toMatchSnapshot();
+  describe("Rendering", () => {
+    it("should render a basic item", () => {
+      const items = allItems.slice(0, 2);
+      expect(shallow(<GenericCarousel items={items} />)).toMatchSnapshot();
+    });
+
+    it("should use the alt attribute if provided", () => {
+      const items = [{ ...allItems[0], alt: "alt text" }];
+      const wrapper = shallow(<GenericCarousel items={items} />);
+      expect(wrapper.find(".gc-img").prop("alt")).toEqual("alt text");
+    });
+
+    it("should fall back to title if there is no alt", () => {
+      const items = [{ ...allItems[0] }];
+      const wrapper = shallow(<GenericCarousel items={items} />);
+      expect(wrapper.find(".gc-img").prop("alt")).toEqual("title 1");
+    });
+
+    it("should use the href attribute if provided", () => {
+      const items = [{ ...allItems[0], href: "href.jpg" }];
+      const wrapper = shallow(<GenericCarousel items={items} />);
+      expect(wrapper.find(".gc-a").prop("href")).toEqual("href.jpg");
+    });
+
+    it("should fall back to src if there is no href", () => {
+      const items = [{ ...allItems[0] }];
+      const wrapper = shallow(<GenericCarousel items={items} />);
+      expect(wrapper.find(".gc-a").prop("href")).toEqual("src-1.jpg");
+    });
+  });
+
+  describe("Navigation controls", () => {
+    const active = "gc-item--active";
+    it("should activate the next item when clicking next", () => {
+      const wrapper = shallow(<GenericCarousel items={allItems} />);
+      const nextButton = wrapper.find(".gc-control-next");
+      nextButton.simulate("click");
+      const secondItem = wrapper.find(".gc-item").at(1);
+      expect(secondItem.hasClass(active)).toBeTruthy();
+    });
+    it("should activate the previous item when clicking prev", () => {
+      const wrapper = shallow(
+        <GenericCarousel items={allItems} initialIndex={1} />
+      );
+      const prevButton = wrapper.find(".gc-control-prev");
+      prevButton.simulate("click");
+      const item = wrapper.find(".gc-item").at(0);
+      expect(item.hasClass(active)).toBeTruthy();
+    });
+    it("should activate the first item when clicking next at the end", () => {
+      const wrapper = shallow(
+        <GenericCarousel items={allItems} initialIndex={6} />
+      );
+      const nextButton = wrapper.find(".gc-control-next");
+      nextButton.simulate("click");
+      const secondItem = wrapper.find(".gc-item").at(0);
+      expect(secondItem.hasClass(active)).toBeTruthy();
+    });
+    it("should activate the last item when clicking prev at the start", () => {
+      const wrapper = shallow(<GenericCarousel items={allItems} />);
+      const prevButton = wrapper.find(".gc-control-prev");
+      prevButton.simulate("click");
+      const item = wrapper.find(".gc-item").at(6);
+      expect(item.hasClass(active)).toBeTruthy();
+    });
+    it("should activate the item when tab-focusing", () => {
+      const wrapper = shallow(<GenericCarousel items={allItems} />);
+      wrapper
+        .find(`a.gc-a`)
+        .at(2)
+        .simulate("focus");
+      const item = wrapper.find(".gc-item").at(2);
+      expect(item.hasClass(active)).toBeTruthy();
+    });
   });
 });
--- packages/generic-carousel/src/generic-carousel.tsx
+++ packages/generic-carousel/src/generic-carousel.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { FC } from "react";
+import React, { FC, useState, useRef } from "react";
 
 export type GCItem = {
   guid: string;
@@ -10,15 +10,41 @@ export type GCItem = {
 };
 export type GenericCarouselProps = {
   items: GCItem[];
+  initialIndex?: number;
 };
 
-export const GenericCarousel: FC<GenericCarouselProps> = props => {
-  const { items } = props;
+export const GenericCarousel: FC<GenericCarouselProps> = ({
+  items,
+  initialIndex = 0
+}) => {
+  const [activeItem, setActiveItem] = useState(initialIndex);
+  const itemsRef = useRef<HTMLDivElement>(null);
+  const showItem = (index: number): void => {
+    setActiveItem(index);
+    const { current } = itemsRef;
+    if (!current) return;
+    const a = current.querySelector<HTMLAnchorElement>(
+      `.gc-item:nth-child(${index + 1}) a`
+    );
+    if (!a) return;
+    a.focus({ preventScroll: true });
+  };
+  const nextItem = (): void => {
+    let next = activeItem + 1;
+    if (next >= items.length) next = 0;
+    showItem(next);
+  };
+  const prevItem = (): void => {
+    let prev = activeItem - 1;
+    if (prev < 0) prev = items.length - 1;
+    showItem(prev);
+  };
   return (
     <section
       aria-label="Generic Carousel"
       aria-roledescription="carousel"
       className="gc-carousel"
+      data-active={activeItem}
     >
       <div className="gc-inner">
         <fieldset className="gc-controls" aria-label="Carousel controls">
@@ -27,6 +53,7 @@ export const GenericCarousel: FC<GenericCarouselProps> = props => {
             className="gc-control gc-control-prev"
             aria-label="Previous"
             aria-controls="abcd-gc-items"
+            onClick={prevItem}
           >
             Prev
           </button>
@@ -34,24 +61,31 @@ export const GenericCarousel: FC<GenericCarouselProps> = props => {
             className="gc-control gc-control-next"
             aria-label="Next"
             aria-controls="abcd-gc-items"
+            onClick={nextItem}
           >
             Next
           </button>
         </fieldset>
-        <div className="gc-items" id="abcd-gc-items">
+        <div className="gc-items" id="abcd-gc-items" ref={itemsRef}>
           {items.map((item, index) => {
             const alt = item.alt || item.title;
             const href = item.href || item.src;
             return (
               <div
                 key={item.guid}
-                className="gc-item"
+                className={`gc-item ${
+                  index === activeItem ? "gc-item--active" : ""
+                }`}
                 aria-label={`Item ${index + 1} of ${items.length}`}
                 aria-roledescription="slide"
                 role="group"
               >
                 <div className="gc-image">
-                  <a className="gc-a" href={href}>
+                  <a
+                    className="gc-a"
+                    href={href}
+                    onFocus={(): void => showItem(index)}
+                  >
                     <img className="gc-img" src={item.src} alt={alt} />
                   </a>
                 </div>
