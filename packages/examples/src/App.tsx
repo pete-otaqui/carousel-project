b6cfdd5d9b8fd1f9f04c28bb219b8f19f094e97d
Pete Otaqui <pete@otaqui.com>
2020-02-22 14:45:48 +0000

    Added naïve data loading to example App

--- packages/examples/src/App.tsx
+++ packages/examples/src/App.tsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import { GenericCarousel } from "@otaqui/generic-carousel";
+import { GenericCarousel, GCItem } from "@otaqui/generic-carousel";
 
 import "./App.css";
 import "@otaqui/generic-carousel/lib/index.css";
 
+const getQueryUrl = (q: string): string => {
+  return `https://pixabay.com/api/?key=9656065-a4094594c34f9ac14c7fc4c39&image_type=photo&q=${q}`;
+};
+
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState<GCItem[]>([]);
+  useEffect(() => {
+    const f = async () => {
+      if (query === "") {
+        return;
+      }
+      const response = await fetch(getQueryUrl(query));
+      const results = await response.json();
+      const items: GCItem[] = results.hits.slice(0, 6).map((result: any) => {
+        return {
+          guid: `${result.id}`,
+          src: result.webformatURL,
+          title: `By ${result.user}, ${result.likes} likes`
+        };
+      });
+      setResults(items);
+    };
+    f();
+  }, [query]);
+
   return (
     <div className="App">
       <header>
@@ -18,7 +44,22 @@ function App() {
         </p>
       </header>
       <main>
-        <GenericCarousel />
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            setQuery(searchValue);
+          }}
+        >
+          <fieldset>
+            <legend>Search</legend>
+            <input
+              value={searchValue}
+              onChange={e => setSearchValue(e.target.value)}
+            />
+            <button type="submit">Search</button>
+          </fieldset>
+        </form>
+        <GenericCarousel items={results} />
       </main>
     </div>
   );
